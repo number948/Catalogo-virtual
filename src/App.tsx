53a35336fc37934b3a8ae47a218022e1d5fe4122
number948 <scottsykotom@gmail.com>
2022-09-18 00:14:05 -0300

    museo_virtual_2_materialUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import  React from 'react';
+import { ThemeProvider } from '@material-ui/core/styles';
+import theme from './temaConfig';
+import { makeStyles } from '@material-ui/core/styles';
+import { Button } from '@material-ui/core';
+import Navbar from './componentes/Navbar';
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+  return ( 
+   
+      <ThemeProvider theme={theme}>
+        
+        <Navbar/>
+
+         <Button variant= 'contained' color= 'primary'>
+            Mi botón personalizado
+        </Button>  
+
+    </ThemeProvider>
+  )
 }
 
 export default App;
